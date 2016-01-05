0b5226aff84101c1707ef77a497d4330add228cf
WhitHet13 <w.hetrick16@gmail.com>
2016-01-05 11:46:20 -0800

    Signed-off-by: WhitHet13 <w.hetrick16@gmail.com>

---
+++ homework/Assignment 1/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,41 @@
+/* 
+    Author: Whitney Hetrick
+    Created on January 4, 2016, 11:40 AM
+    Purpose: Big C
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here 
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    char letter= 'X';
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    
+    //Output results
+    cout<<"    "<<letter<<letter<<letter<<endl;
+    cout<<"  "<<letter<<"    "<<letter<<endl;
+    cout<<""<<letter<<endl;
+    cout<<""<<letter<<endl;
+    cout<<""<<letter<<endl;
+    cout<<""<<letter<<endl;
+    cout<<""<<letter<<endl;
+    cout<<"  "<<letter<<"    "<<letter<<endl;
+    cout<<"    "<<letter<<letter<<letter<<endl;
+    
+  
+    //Exit stage right
+    return 0;
+}
+
